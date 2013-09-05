2d94b2b0dec30dc8991a764dbc9c52e9f37e9291
BenEdick <brge85@gmail.com>
2013-09-04 19:49:00 -0700

    Favorite Movies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @first_movie = Movie.first
     @first_vote_count = @first_movie.votes.count
+    @my_favorite = Movie.find_by title: "Boondock Saints"
   end
 
 end
